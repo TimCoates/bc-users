2e741b2702851b1062462c1172958559982db0f3
Tim Coates <tim.coates@gmail.com>
2022-11-26 08:39:34 +0000

    Changing tests.

--- tests/bc-handler.test.ts
+++ tests/bc-handler.test.ts
@@ -1,11 +1,29 @@
 
 const BCClient = require("../src/bc-handler.ts");
 describe("test constructor", () => {
-	it("should work", () => {
+	it("Should construct", () => {
 		let username = process.env.BC_USERNAME;
 		let password = process.env.BC_PASSWORD;
 		let client = new BCClient.BCClient(username, password);
-		console.log(username);
-		expect(true).toBe(true);
+		expect(client).not.toBe(null);
 	});
+
+});
+
+describe("test authenticate", () => {
+	it("Should not be authenticated initially", () => {
+		let username = process.env.BC_USERNAME;
+		let password = process.env.BC_PASSWORD;
+		let client = new BCClient.BCClient(username, password);
+		expect(client.authenticated).toBe(false);
+	});
+
+	it("Should be authenticated after authenticating", () => {
+		let username = process.env.BC_USERNAME;
+		let password = process.env.BC_PASSWORD;
+		let client = new BCClient.BCClient(username, password);
+		client.authenticate();
+		expect(client.authenticated).toBe(true);
+	});
+
 });
