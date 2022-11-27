c6a017c88626728750ef456abaeaf862fb2444a6
Tim Coates <tim.coates@gmail.com>
2022-11-27 17:34:31 +0000

    Tidied tests a bit.

--- tests/bc-handler.test.ts
+++ tests/bc-handler.test.ts
@@ -41,32 +41,29 @@ describe("test authenticate", () => {
 describe("test getMembers", () => {
 
 	it("Should not get anyone initially", async () => {
-		let people: structures.bcPerson[] = [];
 		let username = process.env.BC_USERNAME;
 		let password = process.env.BC_PASSWORD;
 		let client = new BCClient.BCClient(username, password);
-		people = await client.getMembers(process.env.BC_CLUBID);
+		let people: structures.bcPerson[] = await client.getMembers(process.env.BC_CLUBID);
 		expect(people.length).toBe(0);
 	});
 
 	it("Should get people once authenticated", async () => {
-		let people: structures.bcPerson[] = [];
 		let username = process.env.BC_USERNAME;
 		let password = process.env.BC_PASSWORD;
 		let client = new BCClient.BCClient(username, password);
 		let result = await client.authenticate();
-		people = await client.getMembers(process.env.BC_CLUBID);
+		let people: structures.bcPerson[] = await client.getMembers(process.env.BC_CLUBID);
 		expect(result).toBe(true);
 		expect(people.length).toBeGreaterThan(1);
 	});
 
 	it("Should not get people from a wrong club once authenticated", async () => {
-		let people: structures.bcPerson[] = [];
 		let username = process.env.BC_USERNAME;
 		let password = process.env.BC_PASSWORD;
 		let client = new BCClient.BCClient(username, password);
 		let result = await client.authenticate();
-		people = await client.getMembers("1234");
+		let people: structures.bcPerson[] = await client.getMembers("1234");
 		expect(people.length).toBe(0);
 	});
 });
